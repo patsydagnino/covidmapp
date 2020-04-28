ab94b7c0e40592e59ef4617a0d7d646db78e4c28
Patsy Dagnino <patsy.dagnino94@outlook.com>
2020-04-28 10:08:03 -0400

    project structure

--- app/src/main/java/net/aptivist/covid_mapp/view/MainActivity.kt
+++ app/src/main/java/net/aptivist/covid_mapp/view/MainActivity.kt
@@ -1,13 +1,13 @@
-package net.aptivist.covid_mapp
+package net.aptivist.covid_mapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import net.aptivist.covid_mapp.R
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main
-            khj
+        setContentView(R.layout.activity_main)
     }
 }
